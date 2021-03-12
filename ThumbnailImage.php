2c30a2c1db20640960f331b46d81eb55bf3d23ee
mganesh alkurn <mganesh.alkurn@gmail.com>
2021-03-12 12:59:21 +0530

    Update

--- ThumbnailImage.php
+++ ThumbnailImage.php
@@ -77,85 +77,6 @@ class ThumbnailImage
      */
     public static function thumbnailFile($file, $width, $height, $mode = self::THUMBNAIL_OUTBOUND, $quality = null)
     {
-<?php
-namespace alkurn\thumbnail;
-
-use Yii;
-use yii\helpers\Html;
-use yii\helpers\FileHelper;
-use yii\imagine\Image;
-use Imagine\Image\Box;
-use Imagine\Image\ManipulatorInterface;
-
-/**
- * Yii2 helper for creating and caching thumbnails on real time
- * @author Ganesh
- * @package alkurn\thumbnail
- */
-class ThumbnailImage
-{
-    const THUMBNAIL_OUTBOUND = ManipulatorInterface::THUMBNAIL_OUTBOUND;
-    const THUMBNAIL_INSET = ManipulatorInterface::THUMBNAIL_INSET;
-    const THUMBNAIL_INSET_BOX = 'inset_box';
-    const IMAGE_QUALITY = 90;
-    const MKDIR_MODE = 0755;
-
-
-    /** @var string $cacheAlias path alias relative with @web where the cache files are kept */
-    public static $cacheAlias = 'assets/thumbnails';
-    public static $uploadsAlias = 'assets/thumbnails';
-    public static $imageAlias = 'assets/thumbnails';
-    public static $storageAlias = 'assets/thumbnails';
-    public static $defaultImage = 'default.png';
-
-    /** @var int $cacheExpire */
-    public static $cacheExpire = 0;
-
-    /**
-     * Creates and caches the image thumbnail and returns ImageInterface.
-     *
-     * @param string $filename the image file path or path alias
-     * @param integer $width the width in pixels to create the thumbnail
-     * @param integer $height the height in pixels to create the thumbnail
-     * @param string $mode self::THUMBNAIL_INSET, the original image
-     * is scaled down so it is fully contained within the thumbnail dimensions.
-     * The specified $width and $height (supplied via $size) will be considered
-     * maximum limits. Unless the given dimensions are equal to the original image’s
-     * aspect ratio, one dimension in the resulting thumbnail will be smaller than
-     * the given limit. If self::THUMBNAIL_OUTBOUND mode is chosen, then
-     * the thumbnail is scaled so that its smallest side equals the length of the
-     * corresponding side in the original image. Any excess outside of the scaled
-     * thumbnail’s area will be cropped, and the returned thumbnail will have
-     * the exact $width and $height specified
-     * @return \Imagine\Image\ImageInterface
-     */
-    public static function thumbnail($filename, $width, $height, $mode = self::THUMBNAIL_OUTBOUND, $quality = null)
-    {
-        return Image::getImagine()->open(self::thumbnailFile($filename, $width, $height, $mode, $quality));
-    }
-
-    public static function awsFile($file, $width, $height)
-    {
-
-        $thumbnailFileExt = pathinfo($file, PATHINFO_EXTENSION);
-        $realFilePath = pathinfo($file, PATHINFO_DIRNAME);
-        $thumbnailFileName = pathinfo($file, PATHINFO_FILENAME) . '-' . $width . 'x' . $height;
-        $file = $realFilePath . DIRECTORY_SEPARATOR . $thumbnailFileName . '.' . $thumbnailFileExt;
-        return $file;
-    }
-
-    /**
-     * Creates and caches the image thumbnail and returns full path from thumbnail file.
-     *
-     * @param string $filename
-     * @param integer $width
-     * @param integer $height
-     * @param string $mode
-     * @return string
-     * @throws FileNotFoundException
-     */
-    public static function thumbnailFile($file, $width, $height, $mode = self::THUMBNAIL_OUTBOUND, $quality = null)
-    {
         $cachePath = self::$cacheAlias;
         $file = empty($file) ? self::$defaultImage : $file;
         $filename = FileHelper::normalizePath(self::$uploadsAlias . $file);
@@ -305,4 +226,3 @@ class ThumbnailImage
     }
 
 }
-
